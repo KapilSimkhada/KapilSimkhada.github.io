1d44ab9621655ad25f1c3628c6dbc7f2a38046bb
Kapil Simkhada <90088523+KapilSimkhada@users.noreply.github.com>
2026-09-17 10:09:52 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,31 @@
+const menuToggle =
+    document.getElementById("menu-toggle");
 
+const navLinks =
+    document.querySelector(".nav-links");
+
+
+if (menuToggle) {
+
+    menuToggle.addEventListener(
+        "click",
+        function () {
+
+            navLinks.classList.toggle("active");
+
+        }
+    );
+
+}
+
+
+const year =
+    document.getElementById("year");
+
+
+if (year) {
+
+    year.textContent =
+        new Date().getFullYear();
+
+}
